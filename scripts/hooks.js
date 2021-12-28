0a9adaa1b336ac831df705922bcc8c660cbe4e09
Matthew Monahan <64717385+Kekilla0@users.noreply.github.com>
2021-12-28 15:30:05 -0500

    Update hooks.js

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -18,8 +18,9 @@ Hooks.on('getCompendiumEntryContext', (html, contextOptions) => helper.addContex
   TODO Fixes :
 
   TODO Ideas :
-    add system CyberPunk RED
+    !! NOT PLANNED add system CyberPunk RED !!
     add system Warhammer Fantasy
+    add system Pathfinder 2e
     add capability to update all items in a compendium via a compendium
       !figure out a way to only add context option to items compendium
       !Update All Item Macros
@@ -28,5 +29,7 @@ Hooks.on('getCompendiumEntryContext', (html, contextOptions) => helper.addContex
       !Update All via that Item Macro      
 
   Update Notes :
+    Added Context Menu for Compendium Directory --InProgress
+    Added Context Menu for Compendium Entry --InProgress
 
 */
