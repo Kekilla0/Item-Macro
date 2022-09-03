9fed7ff8690bf1c3cf631789956b0f613ae01351
trioderegion <matt.haentschke@gmail.com>
2022-09-03 14:15:10 -0400

    cross 9/10 functionality for SWS

--- scripts/systems/worldbuilding.js
+++ scripts/systems/worldbuilding.js
@@ -34,7 +34,8 @@ export function register_helper()
     /* If an item was dropped, handle it ourselves */
     if( data.type == 'Item' ) {
       const command = `game.worldbuilding.rollItemMacro('${data.data.name}')`;
-      let macro = game.macros.find(m => (m.name === data.data.name) && (m.command === command));
+      const comparisonName = settings.isV10 ? data.name : data.data.name;
+      let macro = game.macros.find(m => (m.name === comparisonName) && (m.command === command));
       if (!macro) {
         macro = await Macro.create({
           name: data.data.name,
